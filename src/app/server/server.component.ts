2b7896d7f43a0c5bc162eaae00b7e51bcffc8c06
MD. Towhidul Islam <towhidul@1it.com.au>
2021-07-23 15:04:52 +0600

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,14 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector:'app-server',
+  selector: 'app-server',
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'Online';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
