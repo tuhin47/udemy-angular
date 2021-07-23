e533713719464a1fd2a4b9d7c8518c4d51618199
MD. Towhidul Islam <towhidul@1it.com.au>
2021-07-23 15:12:14 +0600

    property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,16 @@ import {Component, OnInit} from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>
-    `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
   constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 5000)
   }
 
   ngOnInit(): void {
