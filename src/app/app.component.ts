3120b3ae6262d1c100e1f1170bddd10c0b24a00f
MD. Towhidul Islam <towhidul@1it.com.au>
2021-07-26 09:12:55 +0600

    project init for shop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
-  styles:[`
-  h3{
-    color: aqua;
-  }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 }
