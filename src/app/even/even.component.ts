8877cf473949141eaa49a33c53a6d19f588c9a20
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-07-30 15:37:32 +0600

    complete the task

--- src/app/even/even.component.ts
+++ src/app/even/even.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-even',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EvenComponent implements OnInit {
 
-  constructor() { }
+  @Input() value: number;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() gameEmitter = new EventEmitter<number>();
 
-  constructor() { }
+  game: number;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  startGame() {
+    let i = 1;
+    this.game = setInterval(() => this.gameEmitter.emit(i++), 1000);
+  }
+
+  stopGame() {
+    clearInterval(this.game);
+  }
 }
