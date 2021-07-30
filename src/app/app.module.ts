a59d86423160622c76733bfd66fe2e45cfd16b28
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-07-30 14:37:33 +0600

    init databinding-assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,19 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import {AppComponent} from './app.component';
-import {ServerComponent} from "./server/server.component";
-import {ServersComponent} from './servers/servers.component';
-import {WarningAlertComponent} from './warning-alert/warning-alert.component';
-import {SuccessAlertComponent} from './success-alert/success-alert.component';
-import {FormsModule} from "@angular/forms";
-import {CounterComponent} from './counter/counter.component';
-import { CounterActionsComponent } from './counter/counter-actions/counter-actions.component';
-import { CounterViewComponent } from './counter/counter-view/counter-view.component';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    CounterComponent,
-    CounterActionsComponent,
-    CounterViewComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
