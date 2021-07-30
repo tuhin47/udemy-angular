06207b6ddcb726743c910bb10aba4cb78ed92da8
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-07-30 16:02:11 +0600

    header navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  featureLoaded = 'recipe';
+  featureSelection(feature: string) {
+    this.featureLoaded = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,13 @@
-import {Component} from "@angular/core";
+import {Component, Output, EventEmitter} from "@angular/core";
 
 @Component({
   selector: 'app-header',
-  templateUrl:'./header.component.html'
+  templateUrl: './header.component.html'
 })
-export class HeaderComponent{}
+export class HeaderComponent {
+  @Output() featureEmit = new EventEmitter<string>();
+
+  onListSelect(feature: string) {
+    this.featureEmit.emit(feature);
+  }
+}
