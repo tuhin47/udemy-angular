6ee1ac37747a08f4a429aa800d22f60605736b62
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-07-31 23:30:50 +0600

    get route params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -16,6 +14,7 @@ import {RouterModule, Routes} from "@angular/router";
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id', component: UserComponent},
   {path: 'servers', component: ServersComponent},
 ];
 @NgModule({
