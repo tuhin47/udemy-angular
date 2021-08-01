15f5fedf3712e891b33514463b162c37fbe1b1b0
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-01 11:57:55 +0600

    nw routes for edit and add

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -4,12 +4,15 @@ import {RecipesComponent} from "./app/recipes/recipes.component";
 import {ShoppingListComponent} from "./app/shopping-list/shopping-list.component";
 import {RecipeStartComponent} from "./app/recipes/recipe-start/recipe-start.component";
 import {RecipeDetailComponent} from "./app/recipes/recipe-detail/recipe-detail.component";
+import {RecipeEditComponent} from "./app/recipes/recipe-edit/recipe-edit.component";
 
 const appRoutes: Routes=[
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent, children:[
       {path:'', component: RecipeStartComponent, pathMatch: 'full'},
+      {path:'new', component: RecipeEditComponent},
       {path:':id', component: RecipeDetailComponent},
+      {path:':id/edit', component: RecipeEditComponent},
     ]},
   {path: 'shopping-list', component: ShoppingListComponent},
 ];
