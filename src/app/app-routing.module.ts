6c0d8c78c68355b408501abeca88442c0d7b8f64
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-01 10:56:24 +0600

    error page with messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import {EditServerComponent} from "./servers/edit-server/edit-server.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AuthGuardService} from "../auth-guard.service";
 import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
+import {ErrorPageComponent} from "./error-page/error-page.component";
 
 
 const appRoutes: Routes = [
@@ -28,8 +29,9 @@ const appRoutes: Routes = [
       {path: ':id/edit', component: EditServerComponent , canDeactivate: [CanDeactivateGuard]}
     ]
   },
-  {path: '404', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/404'}
+  // {path: '404', component: PageNotFoundComponent},
+  {path: 'error', component: ErrorPageComponent , data:{message: 'Page Not Found!'}},
+  {path: '**', redirectTo: '/error'}
 ];
 
 @NgModule({
