6bd439d995a5090b56351cfe0998f0ff7e7c5cee
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-01 13:04:24 +0600

    subject for cross event communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from "./user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit,OnDestroy {
+  userActivated = false;
+  private subscription: Subscription;
+  constructor(private userService:UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.userService.activatedEmitter
+      .subscribe(
+        value => this.userActivated = value
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
