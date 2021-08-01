73f6c544f169f1128186fcd6c88ce5204227965f
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-01 12:00:44 +0600

    same component edit and add

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  private id: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        params =>{
+          this.id = +params['id'];
+          this.editMode = this.id != null;
+        }
+      )
   }
 
 }
