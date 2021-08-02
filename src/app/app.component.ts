c60584357d921f5d6d5a266f1184ff95d2af39c5
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-02 13:12:22 +0600

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  projectForm: FormGroup;
+  projectStatus = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null,
+        [Validators.required, this.forbiddenProjectName.bind(this)],
+        this.forbiddenProjectNameAsync.bind(this)),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+    console.log(this.projectForm.value);
+  }
+
+  forbiddenProjectName(control: FormControl) {
+    if (control.value === 'Test') {
+      return {'forbiddenSync': true}
+    }
+  }
+
+  forbiddenProjectNameAsync(control: FormControl) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value && control.value.includes('test')) {
+          resolve({'forbiddenAsync': true})
+        }
+      }, 1500)
+    })
+  }
+
 }
