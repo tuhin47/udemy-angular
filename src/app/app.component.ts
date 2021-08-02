3b5a27842a8e26aea07c1f68258d1d609b55c096
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-02 12:43:26 +0600

    form-reactive-assignment init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
-  signupForm: FormGroup;
-  forbiddenUsernames = ['Anna', 'Bale'];
-
-  ngOnInit(): void {
-    this.signupForm = new FormGroup({
-      'userData': new FormGroup({
-        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.email, Validators.required], this.forbiddenEmails.bind(this))
-      }),
-      'gender': new FormControl('male'),
-      'hobbies': new FormArray([])
-    });
-    /*this.signupForm.valueChanges.subscribe(
-      value=>{
-        console.log(value);
-      }
-    )*/
-    /*this.signupForm.statusChanges.subscribe(
-      status => console.log(status)
-    );*/
-    this.signupForm.patchValue({
-      'userData':{
-        'username': 'tuhin47'
-      }
-    })
-  }
-
-  onSubmit() {
-    console.log(this.signupForm);
-    this.signupForm.reset();
-  }
-
-  onAddHobby() {
-    const control = new FormControl(null, Validators.required);
-    (<FormArray>this.signupForm.get('hobbies')).push(control);
-  }
-
-  getHobbyControls() {
-    return (<FormArray>this.signupForm.get('hobbies')).controls
-  }
-
-  forbiddenNames(control: FormControl): { [s: string]: boolean; } {
-    if (this.forbiddenUsernames.indexOf(control.value) >= 0) {
-      return {'nameIsForbidden': true};
-    }
-    return null;
-  }
-
-  forbiddenEmails(control: FormControl) {
-    return new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value && control.value.includes('test')) {
-          resolve({'emailIsForbidden': true});
-        } else {
-          resolve(null);
-        }
-      }, 1500);
-    });
-  }
+export class AppComponent {
 }
