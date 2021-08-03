92972dc23d2b77a8965086cb1f27f48c37126f8d
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-03 10:23:23 +0600

    query params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
@@ -29,11 +29,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
         {
-          headers: new HttpHeaders({ 'Custom-Header': 'Hello' })
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          params: searchParams
         }
       )
       .pipe(
