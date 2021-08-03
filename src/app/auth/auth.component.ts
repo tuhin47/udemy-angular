ac31620048099378f4d018e61b2222e1a291902a
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-03 12:20:08 +0600

    form submit and validation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    console.log(authForm);
+    authForm.reset();
+  }
 }
