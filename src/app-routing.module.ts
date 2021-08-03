bbde61e291912fdabf5c14ffa8f03f350d7500dd
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-03 12:11:30 +0600

    add auth page

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -6,23 +6,28 @@ import {RecipeStartComponent} from "./app/recipes/recipe-start/recipe-start.comp
 import {RecipeDetailComponent} from "./app/recipes/recipe-detail/recipe-detail.component";
 import {RecipeEditComponent} from "./app/recipes/recipe-edit/recipe-edit.component";
 import {RecipesResolverService} from "./app/recipes/recipes-resolver.service";
+import {AuthComponent} from "./app/auth/auth.component";
 
-const appRoutes: Routes=[
+const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children:[
-      {path:'', component: RecipeStartComponent},
-      {path:'new', component: RecipeEditComponent},
-      {path:':id', component: RecipeDetailComponent, resolve:[RecipesResolverService]},
-      {path:':id/edit', component: RecipeEditComponent, resolve:[RecipesResolverService]},
-    ]},
+  {
+    path: 'recipes', component: RecipesComponent, children: [
+      {path: '', component: RecipeStartComponent},
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+      {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]},
+    ]
+  },
   {path: 'shopping-list', component: ShoppingListComponent},
+  {path: 'auth', component: AuthComponent}
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule{
+export class AppRoutingModule {
 
 }
