adaa98a79f34ae04398dc1fc8c15c78800cc77a3
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-04 12:11:00 +0600

    refactor position

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {RecipesComponent} from "./app/recipes/recipes.component";
-import {ShoppingListComponent} from "./app/shopping-list/shopping-list.component";
-import {RecipeStartComponent} from "./app/recipes/recipe-start/recipe-start.component";
-import {RecipeDetailComponent} from "./app/recipes/recipe-detail/recipe-detail.component";
-import {RecipeEditComponent} from "./app/recipes/recipe-edit/recipe-edit.component";
-import {RecipesResolverService} from "./app/recipes/recipes-resolver.service";
-import {AuthComponent} from "./app/auth/auth.component";
+import {RecipesComponent} from "./recipes/recipes.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
+import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
+import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
+import {RecipesResolverService} from "./recipes/recipes-resolver.service";
+import {AuthComponent} from "./auth/auth.component";
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
-import {AppRoutingModule} from "../app-routing.module";
+import {AppRoutingModule} from "./app-routing.module";
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {RecipeService} from "./recipes/recipe.service";
