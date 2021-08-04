3b16282821b0de8f04624bdee8abf7a5b4acd10b
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-08-04 12:26:48 +0600

    split recipe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HeaderComponent} from "./header/header.component";
-import {RecipesComponent} from './recipes/recipes.component';
-import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
-import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
-import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {RecipeService} from "./recipes/recipe.service";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from './auth/auth.component';
@@ -24,20 +18,15 @@ import {AlertComponent} from './shared/alert/alert.component';
 import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ToastrModule} from "ngx-toastr";
+import {RecipesModule} from "./recipes/recipes.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeDetailComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -49,6 +38,7 @@ import {ToastrModule} from "ngx-toastr";
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RecipesModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(
       {
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,38 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+
+import {RecipesComponent} from "./recipes.component";
+import {RecipeListComponent} from "./recipe-list/recipe-list.component";
+import {RecipeItemComponent} from "./recipe-list/recipe-item/recipe-item.component";
+import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
+import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
+import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
+
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ]
+})
+export class RecipesModule {
+}
